0d21c6284d41dba2d0a666c23d78793c5a936de8
jon <18676880281@163.com>
2020-02-29 17:16:13 +0800

    适配器模式；建造者模式；抽象工厂；工厂

---
+++ demo/AbstractFactory/AbstractFacory.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: A1-0491
+ * Date: 2020/2/29
+ * Time: 11:16
+ */
+
+namespace demo\Factory;
+
+interface Car
+{
+    public function drive();
+}
+
+class Bus implements Car
+{
+    public function drive()
+    {
+        // TODO: Implement drive() method.
+    }
+}
+
+
